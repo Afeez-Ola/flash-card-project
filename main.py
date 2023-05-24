586b15109658257f82dcd344c299ecf1a20a540e
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-25 00:32:41 +0200

    1. Read the data from the french_words.csv file in the data folder.
    2. Pick a random French word/translation and put the word into the flashcard. Every time you press the ❌ or ✅ buttons, it should generate a new random word to display. e.g.

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from tkinter import *
+import random
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+
 window = Tk()
 window.title("Flash Card Project")
 window.config(background=BACKGROUND_COLOR)
-
+canvas = Canvas(background=BACKGROUND_COLOR, width=800, height=526, highlightthickness=0)
 card_front_image = PhotoImage(file="images/card_front.png")
 card_back_image = PhotoImage(file="images/card_back.png")
-canvas = Canvas(background=BACKGROUND_COLOR, width=800, height=526, highlightthickness=0)
+
 canvas.create_image(410, 273, image=card_front_image)
 language = canvas.create_text(400, 150, font=("Ariel", 40, "italic"), text="French")
 language_word = canvas.create_text(400, 273, font=("Ariel", 60, "bold"), text="trouve")
 canvas.grid(column=1, row=1, padx=50, pady=50)
+# Generating random words.
+def random_word():
+    wordFile = pandas.read_csv("data/french_words.csv")
+    word_list = [word for word in wordFile["French"]]
+    random_choice = random.randint(0, len(word_list))
+    canvas.itemconfig(language_word, text=word_list[random_choice])
+    print(word_list[random_choice])
+
 
 right_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_image, borderwidth=0, highlightthickness=0)
+right_button = Button(image=right_image, borderwidth=0, highlightthickness=0,command=random_word)
 right_button.grid(column=2, row=2, padx=50, pady=50)
 
-wrong_button = Button(image=wrong_image, borderwidth=0, highlightthickness=0)
+wrong_button = Button(image=wrong_image, borderwidth=0, highlightthickness=0,command=random_word)
 wrong_button.grid(column=0, row=2, padx=50, pady=50)
 
 window.mainloop()
