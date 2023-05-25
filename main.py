a8f91f2c1b897f667336e1e6473dd5f7fc5772a6
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-25 23:36:01 +0200

    1. After a delay of 3s (3000ms), the card should flip and display the English translation for the current word.
    2. The card image should change to the card_back.png and the text colour should change to white. The title of the card should change to "English" from "French".

--- main.py
+++ main.py
@@ -2,19 +2,8 @@ from tkinter import *
 import random
 import pandas
 
-BACKGROUND_COLOR = "#B1DDC6"
-
 window = Tk()
-window.title("Flash Card Project")
-window.config(background=BACKGROUND_COLOR)
-canvas = Canvas(background=BACKGROUND_COLOR, width=800, height=526, highlightthickness=0)
-card_front_image = PhotoImage(file="images/card_front.png")
-card_back_image = PhotoImage(file="images/card_back.png")
-
-card_image = canvas.create_image(410, 273, image=card_front_image)
-language = canvas.create_text(400, 150, font=("Ariel", 40, "italic"), text="French")
-language_word = canvas.create_text(400, 273, font=("Ariel", 60, "bold"), text="trouve")
-canvas.grid(column=1, row=1, padx=50, pady=50)
+BACKGROUND_COLOR = "#B1DDC6"
 
 random_choice = random.randint(0, 101)
 wordFile = pandas.read_csv("data/french_words.csv")
@@ -23,7 +12,7 @@ english_wordList = [word for word in wordFile["English"]]
 
 
 def card_reset():
-    window.after_cancel(card_timer())
+    window.after_cancel(timer)
 
 
 def card_flip():
@@ -31,20 +20,28 @@ def card_flip():
     canvas.itemconfig(card_image, image=card_back_image)
     canvas.itemconfig(language, text="English", fill="white")
     canvas.itemconfig(language_word, text=english_wordList[random_choice], fill="white")
-    card_timer()
+    window.after(3000, card_flip)
 
 
-def card_timer():
-    timer = window.after(3000, card_flip)
-    return timer
-
-
-card_timer()
+timer = window.after(3000, card_flip)
 
 
 # Generating the words.
 def next_card():
     canvas.itemconfig(language_word, text=french_wordList[random_choice])
+
+
+
+window.title("Flash Card Project")
+window.config(background=BACKGROUND_COLOR)
+canvas = Canvas(background=BACKGROUND_COLOR, width=800, height=526, highlightthickness=0)
+card_front_image = PhotoImage(file="images/card_front.png")
+card_back_image = PhotoImage(file="images/card_back.png")
+
+card_image = canvas.create_image(410, 273, image=card_front_image)
+language = canvas.create_text(400, 150, font=("Ariel", 40, "italic"), text="French")
+language_word = canvas.create_text(400, 273, font=("Ariel", 60, "bold"), text=french_wordList[random_choice])
+canvas.grid(column=1, row=1, padx=50, pady=50)
 
 right_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
