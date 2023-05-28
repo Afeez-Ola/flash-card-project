8bafd59205c8a8b5fb6721fde79f18c97ebe00ce
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-28 21:16:07 +0200

    1. When the user presses on the ✅ button, it means that they know the current word on the flashcard and that word should be removed from the list of words that might come up.
    e.g. If french_words.csv had only 3 records:
    
    chaque,each
    parlé,speak
    arrivé,come
    After the user has seen parlé,speak  it should be removed from the list of words that can come up again.
    
    2. The updated data should be saved to a new file called words_to_learn.csv

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ import pandas
 window = Tk()
 BACKGROUND_COLOR = "#B1DDC6"
 
-random_choice = random.randint(0, 101)
+random_choice = random.randint(0, 100)
 new_random_choice = 0
 wordFile = pandas.read_csv("data/french_words.csv")
 french_wordList = [word for word in wordFile["French"]]
 english_wordList = [word for word in wordFile["English"]]
 
 words_to_learn = {
-    "French": [],
-    "English": []
+    "French": ["partie"],
+    "English": ["part"]
 }
 
 
 def missed_words():
+    # global words_to_learn
     text = canvas.itemcget(language_word, "text")
     result = wordFile.loc[wordFile["French"] == text, "English"]
     words_to_learn["French"].append(text)
     words_to_learn["English"].append(result.item())
-    print(words_to_learn["French"], words_to_learn["English"])
+    df = pandas.DataFrame.from_dict(words_to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+
+
+def correct_words():
+    text = canvas.itemcget(language_word, "text")
+    english_wordList.remove(text)
+    french_wordList.remove(text)
 
 
 def card_reset():
@@ -59,6 +67,11 @@ def missed_button_commands():
     missed_words()
 
 
+def correct_button_commands():
+    correct_words()
+    next_card()
+
+
 window.title("Flash Card Project")
 window.config(background=BACKGROUND_COLOR)
 canvas = Canvas(background=BACKGROUND_COLOR, width=800, height=526, highlightthickness=0)
@@ -72,7 +85,7 @@ canvas.grid(column=1, row=1, padx=50, pady=50)
 
 right_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_image, borderwidth=0, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, borderwidth=0, highlightthickness=0, command=correct_button_commands)
 right_button.grid(column=2, row=2, padx=50, pady=50)
 
 wrong_button = Button(image=wrong_image, borderwidth=0, highlightthickness=0, command=missed_button_commands)
