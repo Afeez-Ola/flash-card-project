deec7ed3902d91a7bd43aff07bccd8d015e9a1ad
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-30 23:07:52 +0200

    1. When the user presses on the ✅ button, it means that they know the current word on the flashcard and that word should be removed from the list of words that might come up.
    e.g. If french_words.csv had only 3 records:
    
    chaque,each
    parlé,speak
    arrivé,come
    After the user has seen parlé,speak  it should be removed from the list of words that can come up again.
    
    2. The updated data should be saved to a new file called words_to_learn.csv

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import pandas
 window = Tk()
 BACKGROUND_COLOR = "#B1DDC6"
 
-random_choice = random.randint(0, 100)
-new_random_choice = 0
 wordFile = pandas.read_csv("data/french_words.csv")
 french_wordList = [word for word in wordFile["French"]]
 english_wordList = [word for word in wordFile["English"]]
-
+random_choice = random.randint(0, len(wordFile["French"]))
+new_random_choice = 0
 words_to_learn = {
     "French": ["partie"],
     "English": ["part"]
@@ -18,22 +17,33 @@ words_to_learn = {
 
 
 def missed_words():
-    # global words_to_learn
     text = canvas.itemcget(language_word, "text")
     result = wordFile.loc[wordFile["French"] == text, "English"]
     words_to_learn["French"].append(text)
-    words_to_learn["English"].append(result.item())
+    words_to_learn["English"].append(''.join(result))
     df = pandas.DataFrame.from_dict(words_to_learn)
     df.to_csv("data/words_to_learn.csv", index=False)
 
 
 def correct_words():
     text = canvas.itemcget(language_word, "text")
-    if text in english_wordList:
-        english_wordList.remove(text)
-    elif text in french_wordList:
-        french_wordList.remove(text)
-    print(text)
+
+    def word_index():
+        french_list = wordFile["French"].to_list()
+        english_list = wordFile["English"].to_list()
+        text_index = 0
+        if text in french_list:
+            text_index = french_list.index(text) + 1
+        elif text in english_list:
+            text_index = english_list.index(text) + 1
+        return text_index
+
+    if (text in wordFile["English"].to_list()) or (text in wordFile["French"].to_list()):
+        wordFile["French"].pop(word_index())
+        wordFile["English"].pop(word_index())
+        print(len(wordFile["English"]), len(wordFile["French"]))
+        wordFile.to_csv("data/french_words.csv")
+        print("Done!")
 
 
 def card_reset():
